01d6f38f6a4ca74ea9cda7482900ccd8241adbaa
Larry Banks <larrykbanks21@gmail.com>
2024-01-17 13:39:24 -0500

    commented random.c's main

--- print_random.c
+++ print_random.c
@@ -13,8 +13,7 @@ int main()
 
 	printf("Today's random word: ");
 	for(a=0;a<7;a++)
-		// putchar( randchar() );
-		randchar();
+		putchar( randchar() );
 	putchar('\n');
 
 	return(0);
--- random.c
+++ random.c
@@ -1,23 +1,20 @@
 #include "random.h"
 
-int main()
-{
-    int i = 0;
-    while(i < 100){ 
-        printf("%d\n",randchar());
-        i++;
-    }
-}
+// int main()
+// {
+//     int i = 0;
+//     while(i < 100){ 
+//         printf("%d\n",randchar());
+//         i++;
+//     }
+// }
 
 int randchar()
 {
 	//pick a random int using rand function
 	//return that int between range 65 - 90
-    int range = rand() % (90 + 1 - 65) + 65;
-    
-    
-    
-	return range;
+    int ch = rand() % (90 + 1 - 65) + 65;
+	return ch;
 
 
 }
